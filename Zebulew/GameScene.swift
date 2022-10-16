509c6153ae70d486d1c5f30f4abdcd984bb1dec5
Michael Sebsbe <Michael.mulugeta2010@gmail.com>
2022-10-16 13:42:29 -0400

    initial commit

--- Zebulew/GameScene.swift
+++ Zebulew/GameScene.swift
@@ -9,80 +9,131 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    //Nodes
+    var player: SKNode?
+    var joystick: SKNode?
+    var joystickKnob: SKNode?
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var joystickAction = false
+    
+    //how far the knob can go
+    var knobRadius: CGFloat = 50.0
+    let playerScale = 0.331
+    
+    var knobsOGPositon: CGPoint = CGPoint(x: 0, y: 0)
+    
+    //Sprite Engine
+    var previousTimeInterval: TimeInterval = 0
+    var playerIsFacingRight = true
+    let playerSpeed = 4.0
     
     override func didMove(to view: SKView) {
+        player = childNode(withName: "player")
+        joystick = childNode(withName: "joystick")
+        joystickKnob = joystick?.childNode(withName: "knob")
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        if let joystickKnob = joystickKnob{
+            knobsOGPositon = joystickKnob.position
         }
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+}
+
+// MARK: Touches
+
+extension GameScene{
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        // for every touch
+        for touch in touches {
+            if let joystickKnob = joystickKnob {
+                // get the location of the touch
+                let location = touch.location(in: joystick!)
+                // if the location of touch was in the frame of our knob, set joystick action to true
+                joystickAction = joystickKnob.frame.contains(location)
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard let joystick = joystick,
+              let joystickKnob = joystickKnob else {return}
+        // if joystick not touched, return
+        if !joystickAction {return}
+        
+        for touch in touches{
+            print("touch")
+            // register postion of touch
+            let position = touch.location(in: joystick)
+            
+            //how far we dragged the konb
+            let length = sqrt(pow(position.y, 2) + pow(position.x, 2))
+            //the angle of our drag from the center on the knob
+            let angle = atan2(position.y, position.x)
+            
+            //if dragged withtin the radius of how far the knob can go,
+            if knobRadius > length {
+                joystickKnob.position = position
+            } else { // otherwise calculate where in the knob radius to posiont the knob
+                joystickKnob.position = CGPoint(x: cos(angle) * knobRadius, y: sin(angle) * knobRadius)
+                
+            }
+        }
+        
     }
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        print("ended")
+        returnJoysickKnobToCenter()
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+}
+// MARK: Helper functions
+extension GameScene{
+    fileprivate func returnJoysickKnobToCenter() {
+        let moveBackAction = SKAction.move(to: knobsOGPositon, duration: 0.1)
+        moveBackAction.timingMode = .linear
+        joystickKnob?.run(moveBackAction)
+        joystickAction = false
     }
-    
-    
+}
+
+// MARK: GameLoop
+
+extension GameScene{
+    // main event loop of game
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        let deltaTime = currentTime - previousTimeInterval
+        previousTimeInterval = currentTime
+        print(deltaTime)
+        // Player Movement
+        guard let joystickKnob = joystickKnob else {return}
+        
+        //get position fo joystic
+        let xPosition = Double(joystickKnob.position.x)
+        let yPostion = Double(joystickKnob.position.y)
+        //create a displacement by multiplying joystick postion by Delta
+        let displacment = CGVector(dx: xPosition * deltaTime, dy: yPostion * deltaTime)
+        let move = SKAction.move(by: displacment, duration: 0)
+        let faceAction: SKAction!
+        
+        let movingRight = 0 < xPosition
+        let movingLeft = xPosition < 0
+        
+        if movingLeft && playerIsFacingRight {
+            playerIsFacingRight = false
+            let faceMovement = SKAction.scaleX(to: playerScale * -1, duration: 0.0)
+
+            faceAction = SKAction.sequence([move , faceMovement])
+        } else if movingRight && !playerIsFacingRight {
+            playerIsFacingRight = true
+            let faceMovement = SKAction.scaleX(to: playerScale, duration: 0.0)
+            faceAction = SKAction.sequence([move , faceMovement])
+        } else {
+            faceAction = move
+        }
+        player?.run(faceAction)
+        
     }
 }
